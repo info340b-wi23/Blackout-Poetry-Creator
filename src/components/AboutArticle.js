11a0a9a529fb6e36e23dac3665d36bc0c041ae36
amayalw <97205730+amayalw@users.noreply.github.com>
2023-02-22 17:37:07 -0800

    Update AboutArticle.js

---
+++ src/components/AboutArticle.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export function AboutArticle(props) {
+    return(
+        <article>
+            <header>
+                <img src="img/glen-carrie-oHoBIbDj7lo-unsplash.jpg" alt="A bunch of words" width="300px" aria-label="A bunch of words"></img>
+                <figcaption style="text-align: center">
+                    Photo by <cite><a class="text-dark" href="https://unsplash.com/photos/oHoBIbDj7lo">Glen Carrie</a></cite>
+                </figcaption>
+                <h1 style="text-align: center" class="mt-2">What is Blackout Poetry?</h1>
+            </header>
+            <p>Dating back to the 18th century, blackout poetry, also known as redacted poetry, is a unique form of poetry where one takes any piece of text from any source, such as a newspaper article or old book, and "blacks out" some words while leaving untouched only the ones they desire to keep in their poem. The result is often a visually pleasing piece of contrastive art while simultaneously rendering an interesting poem using words already in the given text, both limiting the poet to what they can write but also allowing them to combine words in a way that has never been done before.</p>
+            <h1 style="text-align: center;">Why Make a Blackout Poetry Website?</h1>
+            <p>Traditional blackout poetry requires pen and paper. A digitalized version of blackout poetry means that you don't have to destroy books or waste ink to get creative. By using this platform, you will not only be able to create blackout poems just like you would with physical materials, but you also have the ability to create poems from almost any piece of text, meaning you can create a poem using your favorite book without sacrificing the physical copy. To learn more about how to use our platform, click <a class="text-primary" href="about2.html">here</a>.</p>
+        </article>
+    )
+}
